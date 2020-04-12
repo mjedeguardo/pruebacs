0aec5bfcbb510d77b80ee8ae2adff2c23eee319e
mjedeguardo <58795962+mjedeguardo@users.noreply.github.com>
2020-04-11 22:28:13 -0500

    Add files via upload

---
+++ menu_responsive/menu.js
@@ -0,0 +1,7 @@
+let boton_menu = document.getElementById('boton-menu');
+let menu = document.getElementById('menu');
+boton_menu.addEventListener('click', function() {
+
+    menu.classList.toggle('aparecer');
+
+});
